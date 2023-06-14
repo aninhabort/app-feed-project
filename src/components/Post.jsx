415abc5a7ddc05c9e40838b610132cbaf51ec5de
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2023-06-14 16:48:40 -0300

    Fazendo a parte da estilização do post e criando um formulario

---
+++ src/components/Post.jsx
@@ -0,0 +1,41 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/aninhabort.png" />
+          <div className={styles.authorInfo}></div>
+          <strong>Ana Carolina B. de Magalhães</strong>
+          <span>Front-end Developer</span>
+        </div>
+
+        <time title='14 de Junho de 2023' dateTime='2023-06-14 15:48:30'>Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Escreva um comentário...'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
